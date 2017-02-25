5a327efbda84fa8b3061e3b7f43479a577f71e88
Jimmy <Jimmy@DESKTOP-U2VD8H1>
2017-02-25 17:02:21 +0000

    fold function

---
+++ Fold/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace Fold
+
+type Class1() = 
+    member this.X = "F#"
---
+++ Fold/Script.fsx
@@ -0,0 +1,3 @@
+﻿let timeSpent = [38;83;49;12;74;74;86;82;15;43;24;87;3;49;81]
+
+List.fold(fun s hrs -> s + hrs) 0 timeSpent
