9f9c70fa64afb698cca61c09432d27511d4606cb
Jimmy <Jimmy@DESKTOP-U2VD8H1>
2017-02-25 11:42:13 +0000

    Records - Person

---
+++ Records/Script.fsx
@@ -0,0 +1,26 @@
+﻿let person = ("Mark", "Gray","Man Utd", 22)
+
+type Person = {
+   firstName: string
+   lastName: string
+   favClub: string
+   myAge: int}
+
+let me = {
+   firstName="Mark"
+   lastName="Gray"
+   favClub="Man Utd"
+   myAge=22}
+
+let {firstName=myFirstname} = me
+let {lastName=myLastname} = me
+let {myAge=myAge} = me
+let {favClub=favClub} = me
+
+let myFullName = me.firstName + " " + me.lastName 
+
+me
+
+let temp = { me with myAge = 30}
+
+me
